3375cb9fde0c03c8a2e5b06fdf6b91c698deb3ac
hp-pc <navyashree811@gmail.com>
2022-08-04 16:54:57 +0530

    [Navya]ADDED: Welcome to Employee Wage Programs

---
+++ src/com/bridgelabz/emp/Welcome_EmployeeWageProblems.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.emp;
+
+public class Welcome_EmployeeWageProblems {
+	public static void main(String[] args) {
+		System.out.println(" Welcome to Employee Wage Problems");
+	}
+
+}
