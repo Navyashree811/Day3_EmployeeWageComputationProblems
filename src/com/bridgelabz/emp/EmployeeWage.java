b38d60e940c64853cc94334dcd4293ac8f05ddbc
hp-pc <navyashree811@gmail.com>
2022-08-12 21:16:41 +0530

    [NAVAYA]ADDED : Compute Employee Wage For multiple companies

---
+++ src/com/bridgelabz/emp/EmployeeWage.java
@@ -0,0 +1,47 @@
+/*
+ * UC 8 : Compute Employee Wage for multiple companies.
+ */
+package com.bridgelabz.emp;
+
+public class EmployeeWage {
+	public static void calculateTotalWage(String companyName, int wagePerHr, int maxWorkingDays, int maxWorkingHrs) {
+		final int PART_TIME = 1;
+		final int FULL_TIME = 2;
+		int totalWage = 0;
+		int workingHrs = 0;
+
+		System.out.println("Details of " + companyName + "Employee");
+		System.out.println("------------------------------------------------------");
+		System.out.println("Wage per hour : " + wagePerHr);
+		System.out.println("Maximum working days : " + maxWorkingDays);
+		System.out.println("Maximum working hours :" + maxWorkingHrs);
+		System.out.printf("%5s     %5s     %5s     %5s\n", "Day", "Workinghrs", "Wage", "total working hrs");
+
+		for (int day = 1, totalWorkingHrs = 0; day <= maxWorkingDays
+				&& totalWorkingHrs <= maxWorkingHrs; day++, totalWorkingHrs += workingHrs) {
+
+			int empType = (int) (Math.random() * 100) % 3;
+			switch (empType) {
+			case FULL_TIME:
+				workingHrs = 8;
+				break;
+			case PART_TIME:
+				workingHrs = 0;
+				break;
+			default:
+				workingHrs = 0;
+				break;
+			}
+			int wage = workingHrs * wagePerHr;
+			totalWage += wage;
+			System.out.printf("%5d     %5d          %5d         %5d\n", day, workingHrs, wage, totalWorkingHrs + workingHrs);
+		}
+
+	}
+	public static void main(String[] args) {
+		calculateTotalWage("D-Mart", 8, 15,200);
+		calculateTotalWage("Jio-Mart",10,25,195);
+		calculateTotalWage("BigCash", 6, 24,160);
+		calculateTotalWage("Witzeal",9,22,180);
+	}
+}
