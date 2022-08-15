26919339ed5fe03b1ba0e96b9c0e6e304d1a78fe
hp-pc <navyashree811@gmail.com>
2022-08-15 19:20:05 +0530

    [NAVAYA]ADDED : Save Total Wage For Each Company

--- src/com/bridgelabz/emp/EmployeeWage.java
+++ src/com/bridgelabz/emp/EmployeeWage.java
@@ -1,47 +1,82 @@
 /*
- * UC 8 : Compute Employee Wage for multiple companies.
+ * UC 9 : Ability to save the Total wage for Each company.
  */
 package com.bridgelabz.emp;
 
 public class EmployeeWage {
-	public static void calculateTotalWage(String companyName, int wagePerHr, int maxWorkingDays, int maxWorkingHrs) {
-		final int PART_TIME = 1;
-		final int FULL_TIME = 2;
-		int totalWage = 0;
-		int workingHrs = 0;
+	// class constants
+	static final int PART_TIME = 1;
+	static final int FULL_TIME = 2;
+	// instance constants
+	final String COMPANY_NAME;
+	final int WAGE_PER_HR;
+	final int MAX_WORKING_DAYS;
+	final int MAX_WORKING_HRS;
+	// instance variable
+	int totalWage;
 
-		System.out.println("Details of " + companyName + "Employee");
-		System.out.println("------------------------------------------------------");
-		System.out.println("Wage per hour : " + wagePerHr);
-		System.out.println("Maximum working days : " + maxWorkingDays);
-		System.out.println("Maximum working hours :" + maxWorkingHrs);
-		System.out.printf("%5s     %5s     %5s     %5s\n", "Day", "Workinghrs", "Wage", "total working hrs");
+	EmployeeWage(String companyName, int wagePerHr, int maxWorkingDays, int maxWorkingHrs) {
+		COMPANY_NAME = companyName;
+		WAGE_PER_HR = wagePerHr;
+		MAX_WORKING_DAYS = maxWorkingDays;
+		MAX_WORKING_HRS = maxWorkingHrs;
+		totalWage = 0;
+	}
 
-		for (int day = 1, totalWorkingHrs = 0; day <= maxWorkingDays
-				&& totalWorkingHrs <= maxWorkingHrs; day++, totalWorkingHrs += workingHrs) {
+	int generateEmployeeType() {
+		return (int) (Math.random() * 100) % 3;
+	}
 
-			int empType = (int) (Math.random() * 100) % 3;
-			switch (empType) {
-			case FULL_TIME:
-				workingHrs = 8;
-				break;
-			case PART_TIME:
-				workingHrs = 0;
-				break;
-			default:
-				workingHrs = 0;
-				break;
-			}
-			int wage = workingHrs * wagePerHr;
-			totalWage += wage;
-			System.out.printf("%5d     %5d          %5d         %5d\n", day, workingHrs, wage, totalWorkingHrs + workingHrs);
+	int getWorkingHrs(int empType) {
+		switch (empType) {
+		case FULL_TIME:
+			return 8;
+
+		case PART_TIME:
+			return 4;
+		default:
+			return 0;
 		}
+	}
 
+	void calculateTotalWage() {
+		System.out.println("Computation of total wage of " + COMPANY_NAME + "employee");
+		System.out.println("-------------------------------------------------------------------");
+		System.out.printf("%5s     %5s     %5s     %5s\n", "Day", "Workinghrs", "Wage", "Total working hrs");
+		int workingHrs = 0;
+		for (int day = 1, totalWorkingHrs = 0; day <= MAX_WORKING_DAYS
+				&& totalWorkingHrs <= MAX_WORKING_HRS; day++, totalWorkingHrs += workingHrs) {
+
+			int empType = generateEmployeeType();
+			workingHrs = getWorkingHrs(empType);
+			int wage = workingHrs * WAGE_PER_HR;
+			totalWage += wage;
+			System.out.printf("%5d       %5d      %5d      %5d\n", day, workingHrs, wage, totalWorkingHrs + workingHrs);
+		}
 	}
-	public static void main(String[] args) {
-		calculateTotalWage("D-Mart", 8, 15,200);
-		calculateTotalWage("Jio-Mart",10,25,195);
-		calculateTotalWage("BigCash", 6, 24,160);
-		calculateTotalWage("Witzeal",9,22,180);
+
+	public String toString() {
+		System.out.println("Details of " + COMPANY_NAME + " employee");
+		System.out.println("-----------------------------------------------------");
+		System.out.println("Wage per hour: " + WAGE_PER_HR);
+		System.out.println("Maximum working days:" + MAX_WORKING_DAYS);
+		System.out.println("Maximum working hours:" + MAX_WORKING_HRS);
+		return "Total wage for a month of " + COMPANY_NAME + " employee is " + totalWage + "\n";
 	}
+
+	public static void main(String args[]) {
+		EmployeeWage google = new EmployeeWage("Google", 8, 20, 100);
+		EmployeeWage microsoft = new EmployeeWage("Microsoft", 5, 30, 150);
+		EmployeeWage TCS = new EmployeeWage("TCS", 8, 18, 110);
+
+		google.calculateTotalWage();
+		System.out.println(google);
+
+		microsoft.calculateTotalWage();
+		System.out.println(microsoft);
+
+		TCS.calculateTotalWage();
+		System.out.println(TCS);
+	}
+
 }
